9aaaccf32b2ecf89f13eb0d17a8a823a96899d9a
Kevin Dunne <kevinmdunne@gmail.com>
2014-12-10 22:23:35 +0000

    adding status to the response class

--- data/src/main/java/com/mini/data/MicroserviceResponse.java
+++ data/src/main/java/com/mini/data/MicroserviceResponse.java
@@ -2,6 +2,31 @@ package com.mini.data;
 
 public class MicroserviceResponse extends MicroservicePacket{
 
-	private static final long serialVersionUID = -5585513491387680505L;
-
+	private static final long serialVersionUID = -6500637629805011841L;
+	
+	public static final int SUCCESS = 0;
+	public static final int FAILURE = 1;
+	
+	private int status;
+	private String statusMessage;
+	
+	public MicroserviceResponse(){
+		super();
+	}
+	
+	public int getStatus(){
+		return this.status;
+	}
+	
+	public void setStatus(int status){
+		this.status = status;
+	}
+	
+	public void setStatusMessage(String message){
+		this.statusMessage = message;
+	}
+	
+	public String getStatusMessage(){
+		return statusMessage;
+	}
 }
